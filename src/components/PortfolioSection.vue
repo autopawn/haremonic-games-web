9e1f8959361f708b0a11f67b7413237abe684e28
Rodrigo Escar <escar.rodrigo@gmail.com>
2022-12-30 23:06:49 +0000

    Full width Project Card Base

--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -4,26 +4,106 @@
   <header class="major">
     <h2>Our Projects</h2>
   </header>
-  <div class="row">
-    <div class="col-4 col-6-medium col-12-small"/>
-    <div class="col-4 col-6-medium col-12-small">
-      <section id="hare0" class="box">
-        <a href="#hare0" class="image featured"><img src="@/assets/images/slider.png" alt="" /></a>
-        <header>
-          <h3>Hare 136: Slider</h3>
+  
+  <article id="slider" class="slider portfolio-item">
+  <div class="container portfolio-item">
+      <div class="poster">
+        <img src="@/assets/images/slider/poster.png" class="poster d-block mx-auto text-center" alt="...">
+      </div>
+      <div class="portfolio-item main">
+        <header class="portfolio-item">
+          <h2>Hare 136 - Slider</h2>
+          <p>
+            <b>Hare 136 - Slider</b> is a simple and relaxing but increasingly challenging sliding puzzle game, in which you help cute bunnies get to their home sliding through the world.
+          </p>
         </header>
-        <p>A Relaxing but challenging puzzle game akin to classic sokoban but with cute rabbits.</p>
-        <p>Available both for Web and Android.</p>
-        <footer>
-          <h6>Play on</h6>
-          <ul class="actions">
-            <li><a class="icon brands fa-itch-io" href="https://haremonic-games.itch.io/hare136-slider"><span class="label">Play on Itch.io</span></a></li>
-            <li><a class="icon brands fa-google-play" href="https://play.google.com/store/apps/details?id=games.haremonic.slider"><span class="label">Get on Google Play</span></a></li>
-          </ul>
+        <footer class="portfolio-item">
+          <h5>Play on</h5> 
+          <a href="https://play.google.com/store/apps/details?id=games.haremonic.slider" class="store-btn">
+            <img alt="Get it on Google Play" src="@/assets/images/badges/google-play-badge.png" class="store-btn"/>
+          </a>
+          <a href="https://haremonic-games.itch.io/hare136-slider" class="store-btn">
+            <img alt="Available on itch.io" src="@/assets/images/badges/itch-io-badge.svg" class="store-btn"/>
+          </a>
         </footer>
-      </section>
+      </div>
     </div>
-    <div class="col-4 col-6-medium col-12-small"/>
-  </div>
+</article>
+
+
 </section>
 </template>
+
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Lato");
+@import url('https://fonts.googleapis.com/css2?family=Merriweather');
+
+body {
+  margin: 0;
+	padding: 0;
+  border: 0;
+  vertical-align: baseline;
+}
+
+section#banner {
+  background-image: url('@/assets/images/banner.png');
+  background-position: center top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  min-height: 405px;
+  padding: 8em 0;
+}
+
+
+article#slider {
+  background-image: url('@/assets/images/slider/bg.png');
+}
+
+article.portfolio-item {
+  background-size: cover;
+  padding: 4em;
+}
+
+header.portfolio-item {
+  padding-top: 1em;
+}
+
+.portfolio-item.container {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.portfolio-item.main {
+  flex: 1 1 300px;
+} 
+
+div.poster {
+  float: left;
+  max-width: 300px;
+  margin-right: 40px;
+}
+
+img.poster {
+  width: 100%;
+}
+
+img.store-btn {
+  height: 50px;
+  padding: 10px;
+}
+
+p {
+  font-family: Lato;
+  font-weight: normal;
+}
+
+h1,h2,h3,h4,h5,h6 {
+  font-family: Merriweather;
+  font-weight: bolder;
+}
+</style>
